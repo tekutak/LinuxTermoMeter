bbe19beb072beab4f74713d243fd3f064cee2fc3
tekutak <tekuta.org@gmail.com>
2021-09-12 22:38:18 +0900

    [main]Init分離、コメントメンテナンス

--- src/main.c
+++ src/main.c
@@ -10,24 +10,29 @@
 #include "Paint.h"
 #include "Thermo.h"
 
+/* -----------------------------------------------------------------------------
+ Prototype
+------------------------------------------------------------------------------*/
+BOOL Init();
 
+/* -----------------------------------------------------------------------------
+ Function   : main
+ Memo       : 
+ Date       : 2021.09.12
+------------------------------------------------------------------------------*/
 int main()
 {
 	BOOL ret;
 
 	/* 初期化 */
-	ret = I2cCtl_Init();
-	ret = Bme280_Init();
-	ret = Ssd1306_Init();
-	ret = Paint_Init();
-	ret = Thermo_Init();
-
+	ret = Init();
 	if (ret == NG)
 	{
-		printf("[BME280]Init Error.\n");
+		printf("Init Error.\n");
 		return -1;
 	}
 
+	/* 温度計 */
 	for (;;)
 	{
 		Thermo_Measure();
@@ -35,15 +40,58 @@ int main()
 		F32 t = Thermo_Get_Temperature();
 		F32 h = Thermo_Get_Humidity();
 		F32 p = Thermo_Get_Pressure();
-		printf("Temperature:%f\n", t);
-		printf("Humidity:%f\n", h);
-		printf("Pressure:%f\n", p);
+		printf("Temperature:%.1f\n", t);
+		printf("Humidity:%.1f\n", h);
+		printf("Pressure:%.1f\n", p);
 
 		Paint_Draw_ThermoMeter(t, h, p);
 		Paint_Flush_Canvas();
 
-		// sleep(3);
 	}
 
 	return 0;
+}
+
+/* -----------------------------------------------------------------------------
+ Function   : Init
+ Memo       : 
+ Date       : 2021.09.12
+------------------------------------------------------------------------------*/
+BOOL Init()
+{
+	BOOL ret;
+
+	/* 初期化 */
+	ret = I2cCtl_Init();
+	if (ret == NG)
+	{
+		printf("I2cCtl_Init Error.\n");
+		return NG;
+	}
+	ret = Bme280_Init();
+	if (ret == NG)
+	{
+		printf("Bme280_Init Error.\n");
+		return NG;
+	}
+	ret = Ssd1306_Init();
+	if (ret == NG)
+	{
+		printf("Ssd1306_Init Error.\n");
+		return NG;
+	}
+	ret = Thermo_Init();
+	if (ret == NG)
+	{
+		printf("Thermo_Init Error.\n");
+		return NG;
+	}
+	ret = Paint_Init();
+	if (ret == NG)
+	{
+		printf("Paint_Init Error.\n");
+		return NG;
+	}
+
+	return ret;
 }
